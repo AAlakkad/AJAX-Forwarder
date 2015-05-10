2c274459780b8647f2e1c28a1bde0101c0b40fcf
Ammar Alakkad <am.alakkad@gmail.com>
2015-05-10 15:59:00 +0300

    Config - routes: change the route to correspond to any verb/string after base verb

--- config/routes.php
+++ config/routes.php
@@ -1,8 +1,11 @@
 <?php
 $configKey = "forwarder";
+$verb      = Config::get("{$configKey}.route.verb");
 
-Route::get(Config::get("{$configKey}.route.verb"), [
-    'as'         => 'forwarder',
-    'middleware' => Config::get("{$configKey}.middleware"),
-    'uses'       => Config::get("{$configKey}.route.action"),
-]);
+Route::group(['prefix' => $verb], function () use ($configKey) {
+    Route::get('{any?}', [
+        'as'         => 'forwarder',
+        'middleware' => Config::get("{$configKey}.middleware"),
+        'uses'       => Config::get("{$configKey}.route.action"),
+    ])->where('any', '.*');
+});
