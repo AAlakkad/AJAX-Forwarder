6daa138a14a1cb3ad2c973504cb3a0c7847f07ee
Ammar Alakkad <am.alakkad@gmail.com>
2015-05-10 16:00:19 +0300

    Repository - Api: implement sendRequest, and initialize Guzzle Client in the contructor

--- src/AjaxForwarder/Repositories/ApiGuzzle.php
+++ src/AjaxForwarder/Repositories/ApiGuzzle.php
@@ -1,10 +1,31 @@
 <?php
 namespace AAlakkad\AjaxForwarder\Repositories;
 
+use Config;
+
 class ApiGuzzle implements ApiRepository
 {
-    public function sendRequest(array $data)
-    {
+    protected $client;
 
+    public function __construct()
+    {
+        $configKey = "forwarder";
+        $base_url  = Config::get("{$configKey}.servers.default.host");
+
+        // Setup guzzle client with base url.
+        $this->client = new \GuzzleHttp\Client(
+            [
+                'base_url' => $base_url,
+            ]
+        );
+    }
+
+    public function sendRequest($path, array $data = [])
+    {
+        if (count($data)) {
+            $path .= '?';
+            $path .= http_build_query($data);
+        }
+        return $response = $this->client->get($path);
     }
 }
