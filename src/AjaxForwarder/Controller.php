4d0f4c7a7da8326c6c417bbf1fbb89f226ae9f40
Ammar Alakkad <am.alakkad@gmail.com>
2015-05-10 12:19:01 +0300

    Controller: inject ApiRepository

--- src/AjaxForwarder/Controller.php
+++ src/AjaxForwarder/Controller.php
@@ -1,6 +1,7 @@
 <?php
 namespace AAlakkad\AjaxForwarder;
 
+use AAlakkad\AjaxForwarder\Repositories\ApiRepositoriy;
 use Illuminate\Foundation\Bus\DispatchesCommands;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
@@ -8,6 +9,13 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use DispatchesCommands, ValidatesRequests;
+
+    protected $api;
+
+    public function __construct(ApiRepositoriy $api)
+    {
+        $this->api = $api;
+    }
 
     public function handelGet()
     {
