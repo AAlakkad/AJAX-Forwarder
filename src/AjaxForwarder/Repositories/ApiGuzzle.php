dc1ec8f46f100556c28a8a468f65b079494f6983
Ammar Alakkad <am.alakkad@gmail.com>
2015-05-12 16:39:03 +0300

    Repositories - Api: add timeout/connect_timeout options for get() method

--- src/AjaxForwarder/Repositories/ApiGuzzle.php
+++ src/AjaxForwarder/Repositories/ApiGuzzle.php
@@ -25,12 +25,16 @@ class ApiGuzzle implements ApiRepository
         // Get default args from config
         $configKey   = "forwarder";
         $defaultArgs = Config::get("{$configKey}.servers.default.data", []);
+        $options     = [
+            'timeout'         => Config::get("{$configKey}.timeout", 5),
+            'connect_timeout' => Config::get("{$configKey}.connect_timeout", 3),
+        ];
         // Append default args
         $data = array_merge($defaultArgs, $data);
         if (count($data)) {
             $path .= '?';
             $path .= http_build_query($data);
         }
-        return $response = $this->client->get($path);
+        return $response = $this->client->get($path, $options);
     }
 }
