a5c03716ad0686de97a1876cc3e9ce1efc1373da
Ammar Alakkad <am.alakkad@gmail.com>
2015-05-12 17:05:43 +0300

    Controller: catch exceptions when sending requests

--- src/AjaxForwarder/Controller.php
+++ src/AjaxForwarder/Controller.php
@@ -2,10 +2,13 @@
 namespace AAlakkad\AjaxForwarder;
 
 use AAlakkad\AjaxForwarder\Repositories\ApiRepository;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ConnectException;
 use Illuminate\Foundation\Bus\DispatchesCommands;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use \Auth;
+use \Exception;
 use \Log;
 use \Request;
 use \Response;
@@ -30,11 +33,33 @@ class Controller extends BaseController
         }
         // get AJAX request data
         $data = Request::all();
-        // pass the request to the remote server
-        $response    = $this->api->sendRequest($path, $data);
-        $contentType = $response->getHeader('Content-Type');
-        $content     = $response->getBody()->getContents();
-        $status      = $response->getStatusCode();
+        try {
+            // pass the request to the remote server
+            $response    = $this->api->sendRequest($path, $data);
+            $contentType = $response->getHeader('Content-Type');
+            $content     = $response->getBody()->getContents();
+            $status      = $response->getStatusCode();
+        } catch (Exception $e) {
+            $contentType = 'application/json';
+            if ($e instanceof ClientException) {
+                $errorMessage = 'Not found.';
+                $status       = $e->getCode();
+            } elseif ($e instanceof ConnectException) {
+                $errorMessage = 'Connection timeout';
+                $status       = 408;
+            } elseif (isset($e->response) and $e->response !== null) {
+                $status = $e->response->getStatusCode();
+            } else {
+                $status       = 400;
+                $errorMessage = $e->getMessage();
+            }
+
+            $content = json_encode([
+                'error'   => true,
+                'status'  => $status,
+                'message' => $errorMessage,
+            ]);
+        }
 
         // Log requests
         $logData = [
