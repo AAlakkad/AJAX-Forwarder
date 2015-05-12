0c6825b6910d2193fc4a7ef094a0dca493aa1348
Ammar Alakkad <am.alakkad@gmail.com>
2015-05-12 16:35:39 +0300

    Config - Forwarder: add connect_timeout option

--- config/forwarder.php
+++ config/forwarder.php
@@ -1,12 +1,13 @@
 <?php
 return [
-    'route'      => [
+    'route'           => [
         'verb'   => 'ajax',
         'action' => 'AAlakkad\AjaxForwarder\Controller@handleGet',
     ],
-    'middleware' => ['auth'],
-    'timeout'    => 5,
-    'servers'    => [
+    'middleware'      => ['auth'],
+    'timeout'         => 5,
+    'connect_timeout' => 3,
+    'servers'         => [
         'default' => [
             'host' => 'http://jsonplaceholder.typicode.com',
             'data' => [],
